f48fda74d15fbc9d6758cea883ced05923e9e879
jonathan vanasco <jonathan@2xlp.com>
2019-04-17 12:01:24 -0400

    v0.0.7 candidate

--- pyramid_csrf_multi_scheme/__init__.py
+++ pyramid_csrf_multi_scheme/__init__.py
@@ -10,7 +10,7 @@ from pyramid.util import strings_differ
 from pyramid.util import SimpleSerializer
 
 
-__VERSION__ = '0.0.6'
+__VERSION__ = '0.0.7'
 
 
 # ==============================================================================
